6bd7149fd22ec9ea0f534c1970234198effcaf39
Tatiana Kramarenko <tanya19101996@gmail.com>
2023-04-07 13:00:02 +0300

    task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,12 +11,16 @@ import {
   SectionTitle,
 } from './App.styled';
 import { Filter } from './Filter/Filter';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContacts } from 'redux/operations';
+import { selectIsLoading } from 'redux/selectors';
+import { RotatingLines } from 'react-loader-spinner';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  // const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -33,7 +37,11 @@ export const App = () => {
         </Section>
         <Section>
           <SectionTitle>Contacts</SectionTitle>
+
           <Filter />
+          {isLoading && (
+            <RotatingLines height="45" width="45" strokeColor="grey" />
+          )}
           <ContactList />
         </Section>
       </SectionStyle>
